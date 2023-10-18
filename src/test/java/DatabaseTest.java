c0961ad1fb1c64858e4760fa6376b8ccc382df5d
“Slastenov <vtb4055018@vtb.ru>
2023-10-18 19:19:10 +0300

    added DatabaseTest

---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import praktikum.Database;
+
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Тесты для класса /src/main/java/Database.
+ */
+public class DatabaseTest {
+
+    private final Database db = new Database();
+
+    @Test
+    public void availableBunsReturnedListOfBuns() {
+        assertFalse(db.availableBuns().isEmpty());
+    }
+
+    @Test
+    public void availableIngredientsReturnedListOfIngredients() {
+        assertFalse(db.availableIngredients().isEmpty());
+    }
+}
